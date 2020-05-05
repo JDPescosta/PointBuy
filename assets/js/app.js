070f0d5532bb3a31856f7e60d06fd5f6b59ad0fd
JDPescosta <jamesdpescosta@gmail.com>
2020-05-05 14:17:49 -0700

    setup phx for react & added eslint

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from "../css/app.scss"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,10 +12,10 @@ import css from "../css/app.css"
 import "phoenix_html"
 import React, { Component } from "react"
 import ReactDOM from "react-dom"
-import Test from "./components/Test"
+import Home from "./components/Home/Home"
 
 ReactDOM.render(
-  <Test/>, document.getElementById("root")
+  <Home/>, document.getElementById("root")
 )
 // Import local files
 //
